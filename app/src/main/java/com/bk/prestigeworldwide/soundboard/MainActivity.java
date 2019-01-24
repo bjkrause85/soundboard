e0db0a5b26d4eaa447a54c9b0e58ea23b389dc49
Brian Krause <briankrause@Brians-MacBook-Pro.local>
2019-01-23 22:16:06 -0600

    added more sopund files

--- app/src/main/java/com/bk/prestigeworldwide/soundboard/MainActivity.java
+++ app/src/main/java/com/bk/prestigeworldwide/soundboard/MainActivity.java
@@ -30,14 +30,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Fuck You Adany", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Fuck You Adany", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
 
         mToolbar = findViewById(R.id.toolbar);
@@ -49,36 +49,22 @@ public class MainActivity extends AppCompatActivity {
         mSoundList = new ArrayList<>();
         mSoundData = new SoundData("Fuck U Adany", "Tracy - Fuck U Adany", R.drawable.play2, R.raw.fuck_you_adani);
         mSoundList.add(mSoundData);
-        mSoundData = new SoundData("Rejected", "Rejected", R.drawable.play2, R.raw.fuck_you_adani);
+        mSoundData = new SoundData("Rejected", "Rejected", R.drawable.play2, R.raw.rejected);
         mSoundList.add(mSoundData);
-        mSoundData = new SoundData("Not Even Close", "U Like That Jer?", R.drawable.play2, R.raw.fuck_you_adani);
+        mSoundData = new SoundData("Not Even Close", "U Like That Jer?", R.drawable.play2, R.raw.not_even_close);
         mSoundList.add(mSoundData);
-        mSoundData = new SoundData("Under Your Nose", "U Like That Jer?", R.drawable.play2, R.raw.fuck_you_adani);
+        mSoundData = new SoundData("U Like That Jer?", "U Like That Jer?", R.drawable.bear, R.raw.like_that_jer);
         mSoundList.add(mSoundData);
-        mSoundData = new SoundData("U Like That Jer?", "U Like That Jer?", R.drawable.play2, R.raw.fuck_you_adani);
+        mSoundData = new SoundData("Jer? Little Bitch", "U Like That Jer?", R.drawable.bear, R.raw.jer_little_bitch);
         mSoundList.add(mSoundData);
-//        mSoundData = new SoundData("Daisy", getString(R.string.description_flower_daisy),
-//                R.drawable.play);
-//        mSoundList.add(mSoundData);
-//        mSoundData = new SoundData("Sunflower", getString(R.string.description_flower_sunflower),
-//                R.drawable.play);
-//        mSoundList.add(mSoundData);
-//        mSoundData = new SoundData("Daffodil", getString(R.string.description_flower_daffodil),
-//                R.drawable.play);
-//        mSoundList.add(mSoundData);
-//        mSoundData = new SoundData("Gerbera", getString(R.string.description_flower_gerbera),
-//                R.drawable.play);
-//        mSoundList.add(mSoundData);
-//        mSoundData = new SoundData("Orchid", getString(R.string.description_flower_orchid),
-//                R.drawable.play);
-//        mSoundList.add(mSoundData);
-//        mSoundData = new SoundData("Iris", getString(R.string.description_flower_iris),
-//                R.drawable.play);
-//        mSoundList.add(mSoundData);
-//        mSoundData = new SoundData("Lilac", getString(R.string.description_flower_lilac),
-//                R.drawable.play);
-//        mSoundList.add(mSoundData);
-
+        mSoundData = new SoundData("Poppin' Jerry's", "U Like That Jer?", R.drawable.bear, R.raw.poppin_jerrys);
+        mSoundList.add(mSoundData);
+        mSoundData = new SoundData("BK is an idiot", "U Like That Jer?", R.drawable.bk, R.raw.bk_idiot);
+        mSoundList.add(mSoundData);
+        mSoundData = new SoundData("BK bitch", "U Like That Jer?", R.drawable.bk, R.raw.get_ready_bk);
+        mSoundList.add(mSoundData);
+        mSoundData = new SoundData("Andrew = Trash", "U Like That Jer?", R.drawable.bk, R.raw.andrew_is_trash);
+        mSoundList.add(mSoundData);
         MyAdapter myAdapter = new MyAdapter(MainActivity.this, mSoundList);
         mRecyclerView.setAdapter(myAdapter);
     }
